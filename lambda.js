227db7aed47297f21ce6beee5f224df5516b7387
Amanda Gross <amanda.gross6@gmail.com>
2018-01-03 09:36:26 -0500

    Updated successfully posts to DynamoDB

--- lambda.js
+++ lambda.js
@@ -1,11 +1,11 @@
-// TODO: create a time conversion function, alter parsing of the event object to correspond to our webhook
+// TODO: alter parsing of the event object to correspond to our webhook
 //
 // This lambda function receives a JSON event object from the Webhook's POST request, processes the data, and inserts it into the table in DynamoDB.
 // By Amanda Gross
 //
 // Based on a lambda function created by Nic Jansa: https://github.com/nicjansma/dht-logger/blob/master/lambda.js
 //
-
+console.log("hello");
 //
 // Imports
 //
@@ -18,29 +18,39 @@ var TABLE_NAME = "Particle-Photon-Data";
 
 var dynamodb = new doc.DynamoDB();
 
-exports.handler = function(event, context) {
+exports.handler = function(event, context, callback) {
     console.log("Request received:\n", JSON.stringify(event));
     console.log("Context received:\n", JSON.stringify(context));
 
-    var now = new Date().getTime();
+    var eventData = JSON.parse(event.body);
+    var item = {};
 
-    //We can initially determine the device value, which is stored in this item that will be posted to the table.
-    var item = {
-        "device": event.device, 
-    };
-
-
-    //store deviceId and event values
-    if (typeof event.coreid !== "undefined") {
-        item.deviceId = event.coreid;
+    if (typeof eventData.device !== "undefined") {
+        item.device = eventData.device;
+    } else {
+        item.device = "Demo-build";
+    }
+    
+    if (typeof eventData.published_at !== "undefined") {
+        item.time = eventData.published_at;
     }
 
-    if (typeof event.event !== "undefined") {
-        item.event = event.event;
+    //store deviceId and event values
+    if (typeof eventData.coreid !== "undefined") {
+        item.deviceId = eventData.coreid;
+    }
+
+    if (typeof eventData.event !== "undefined") {
+        item.event = eventData.event;
+    }
+    
+    if (typeof eventData.data !== "undefined") {
+        item.data = eventData.data;
     }
 
     // parse data payload
-    var data;
+/*    var data;
+
 
     // if there is a 'data' property, it's possibly from a Particle Webhook, and we should use those values
     if (typeof event.data !== "undefined" && typeof event.published_at !== "undefined") {
@@ -61,7 +71,7 @@ exports.handler = function(event, context) {
             data[key] !== "") {
             item[key] = data[key];
         }
-    }
+    } */
 
     console.log("Item:\n", item);
 
@@ -73,8 +83,14 @@ exports.handler = function(event, context) {
         if (err) {
             context.fail("ERROR: Dynamo failed: " + err);
         } else {
-            console.log("Dynamo Success: " + JSON.stringify(data, null, "  "));
-            context.succeed({ success: true });
+            console.log("Successfully entered to DynamoDB");
+            
+            var response = {
+                "statusCode": 200,
+                "headers": {},
+                "body": JSON.stringify(eventData)
+            };
+            context.succeed(response);
         }
     });
 }
